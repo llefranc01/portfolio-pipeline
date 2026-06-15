68c3334eb7119fd639ad4ef5295e16bfdace5d37
llefranc01 <37572375+llefranc01@users.noreply.github.com>
2026-06-14 21:01:08 -0700

    removed extra h1s

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 export default function About() {
   return (
     <div className="page">
-      <h1>About</h1>
       <p>About me and my work.</p>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 export default function Home() {
   return (
     <div className="page">
-      <h1>Home</h1>
       <p>Welcome to my portfolio.</p>
     </div>
   )
