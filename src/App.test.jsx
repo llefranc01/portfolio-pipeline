c8ac06f464eb8b8f852d94349130bfa164a8c60a
llefranc01 <37572375+llefranc01@users.noreply.github.com>
2026-06-14 20:56:46 -0700

    Update App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,14 @@
 import { test, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 test('renders a heading', () => {
-  render(<App />)
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
   const heading = screen.getByRole('heading')
   expect(heading).toBeInTheDocument()
 })
